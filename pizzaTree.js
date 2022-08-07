c35eab5a0cfa7f4e84bcaab5767b0820c19c404c
ahyoume <aymsmd54@gmail.com>
2022-08-07 16:37:21 +0900

    finish drawing a christmas tree

--- pizzaTree.js
+++ pizzaTree.js
@@ -1,5 +1,13 @@
+const width = 80;
+const height = 40;
 
-for (i = 0; i < 4; i++) {
- console.log("#");
+for (i = 0; i < height; i++) {
+  let line = "";
+  for (j = 0; j < width; j++) {
+    if (j === width / 2) line += "#";
+    else if (j > (width / 2) + i) line += " ";
+    else if (j < (width / 2) - i) line += " ";
+    else line += "#";
  }
-
+console.log(line);
+}
