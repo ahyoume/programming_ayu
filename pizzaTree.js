c1b3a932f1e5f1c4af9a917f5e0ea023d597d64a
ahyoume <aymsmd54@gmail.com>
2022-08-08 22:06:05 +0900

    put pizza on the tree

--- pizzaTree.js
+++ pizzaTree.js
@@ -4,7 +4,8 @@ const height = 40;
 for (i = 0; i < height; i++) {
   let line = "";
   for (j = 0; j < width; j++) {
-    if (j === width / 2) line += "#";
+    if (j === width / 2 && i === 0) line += "🍕";
+    else if (j === width / 2) line += "#";
     else if (j > (width / 2) + i) line += " ";
     else if (j < (width / 2) - i) line += " ";
     else line += "#";
